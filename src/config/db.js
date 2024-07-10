12e646b21f8ae16839377753f31c63033cfc5847
Dolapo Mosuro <mosurodolapo@gmail.com>
2024-07-10 12:26:46 -0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const pg = require("pg");
 
 const sequelize = new Sequelize(
-    process.env.DATABASE_NAME || verceldb,
+    process.env.DATABASE_NAME,
     process.env.DATABASE_USER || default,
     process.env.DATABASE_PASSWORD || lOKBchDd2xU8,
     {
-        host: process.env.DATABASE_HOST || ep-super-bush-a4gsun3t.us-east-1.aws.neon.tech,
+        host: 'ep-super-bush-a4gsun3t.us-east-1.aws.neon.tech',
         dialect: "postgres",
         dialectModule: pg,
         port: process.env.DATABASE_PORT,
