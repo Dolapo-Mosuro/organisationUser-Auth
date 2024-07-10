806342603ff4103aceb1fafb582be9617440a745
Dolapo Mosuro <mosurodolapo@gmail.com>
2024-07-10 12:34:14 -0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,27 +1,11 @@
 require("dotenv").config();
 
 const config = {
-	development: {
-		username: process.env.DATABASE_USER,
-		password: process.env.DATABASE_PASSWORD,
-		database: process.env.DATABASE_NAME,
-		host: process.env.DATABASE_HOST,
-		port: process.env.DATABASE_PORT,
-		dialect: "postgres",
-	},
-	test: {
-		username: process.env.DATABASE_USER,
-		password: process.env.DATABASE_PASSWORD,
-		database: process.env.TEST_DATABASE_NAME,
-		host: process.env.DATABASE_HOST,
-		port: process.env.DATABASE_PORT,
-		dialect: "postgres",
-	},
 	production: {
-		username: process.env.DATABASE_USER,
-		password: process.env.DATABASE_PASSWORD,
-		database: process.env.PRODUCTION_DATABASE_NAME,
-		host: process.env.DATABASE_HOST,
+		username: default,
+		password: lOKBchDd2xU8,
+		database: verceldb,
+		host: ep-super-bush-a4gsun3t.us-east-1.aws.neon.tech,
 		dialect: "postgres",
 	},
 };
