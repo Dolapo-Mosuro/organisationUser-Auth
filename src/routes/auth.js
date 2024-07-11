5b3944a76f895a86be0c821c5a8d5b49e22220b6
dolapo-mosuro <mosurodolapo@gmail.com>
2024-07-11 02:33:42 +0100

    db

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -16,131 +16,130 @@ router.post("/register", async (req, res) => {
 
 	// Validate input fields
 	if (!firstName || !lastName || !email || !password) {
-		// return res.status(422).json({
-		// 		errors: [
-		// 			{
-		// 				field: !firstName
-		// 					? "firstName"
-		// 					: !lastName
-		// 					? "lastName"
-		// 					: !email
-		// 					? "email"
-		// 					: "password",
-		// 				message: "This field is required",
-		// 			},
-		// 		],
-		// 	});
-		// }
-
-		try {
-			const hashedPassword = await bcrypt.hash(password, 10);
-
-			const userId = generateUniqueId("user");
-
-			const user = await User.create({
-				userId,
-				firstName,
-				lastName,
-				email,
-				password: hashedPassword,
-				phone,
-			});
-
-			const defaultOrg = await Organisation.create({
-				OrganisationId: `org-${userId}`,
-				name: `Default Organization for ${userId}`,
-				description: `This is the default organization for ${firstName} ${lastName}`,
-			});
-
-			await user.addOrganisation(defaultOrg);
-
-			const token = jwt.sign({ userId: user.id }, process.env.JWT_KEY, {
-				expiresIn: "1h",
-			});
-
-			res.status(201).json({
-				status: "success",
-				message: "Registration successful",
-				data: {
-					accessToken: token,
-					user: {
-						userId: user.userId,
-						firstName: user.firstName,
-						lastName: user.lastName,
-						email: user.email,
-						phone: user.phone,
-					},
+		return res.status(422).json({
+			errors: [
+				{
+					field: !firstName
+						? "firstName"
+						: !lastName
+						? "lastName"
+						: !email
+						? "email"
+						: "password",
+					message: "This field is required",
 				},
-			});
-		} catch (error) {
-			console.error("Registration error:", error);
-			res.status(400).json({
-				status: "Bad request",
-				message: "Registration unsuccessful",
-				statusCode: 400,
-			});
-		}
+			],
+		});
 	}
 
-	// Login Route
-	router.post("/login", async (req, res) => {
-		const { email, password } = req.body;
+	try {
+		const hashedPassword = await bcrypt.hash(password, 10);
 
-		try {
-			// Check if user exists
-			const user = await User.findOne({ where: { email } });
+		const userId = generateUniqueId("user");
 
-			if (!user) {
-				return res.status(401).json({
-					status: "Bad request",
-					message: "Authentication failed",
-					statusCode: 401,
-				});
-			}
+		const user = await User.create({
+			userId,
+			firstName,
+			lastName,
+			email,
+			password: hashedPassword,
+			phone,
+		});
 
-			// Validate password
-			const hashedPassword = await bcrypt.hash(password, 10);
-			const isPasswordValid = await bcrypt.compare(password, user.password);
+		const defaultOrg = await Organisation.create({
+			OrganisationId: `org-${userId}`,
+			name: `Default Organization for ${userId}`,
+			description: `This is the default organization for ${firstName} ${lastName}`,
+		});
 
-			if (!isPasswordValid) {
-				return res.status(401).json({
-					status: "Bad request",
-					message: "Authentication failed",
-					statusCode: 401,
-				});
-			}
+		await user.addOrganisation(defaultOrg);
 
-			// Generate JWT token
-			const token = jwt.sign(
-				{ userId: user.userId, email: user.email },
-				process.env.JWT_KEY,
-				{ expiresIn: "1h" }
-			);
+		const token = jwt.sign({ userId: user.id }, process.env.JWT_KEY, {
+			expiresIn: "1h",
+		});
 
-			// Response with token and user data
-			res.status(200).json({
-				status: "success",
-				message: "Login successful",
-				data: {
-					accessToken: token,
-					user: {
-						userId: user.userId,
-						firstName: user.firstName,
-						lastName: user.lastName,
-						email: user.email,
-						phone: user.phone,
-					},
+		res.status(201).json({
+			status: "success",
+			message: "Registration successful",
+			data: {
+				accessToken: token,
+				user: {
+					userId: user.userId,
+					firstName: user.firstName,
+					lastName: user.lastName,
+					email: user.email,
+					phone: user.phone,
 				},
-			});
-		} catch (error) {
-			console.error("Login error:", error);
-			res.status(401).json({
+			},
+		});
+	} catch (error) {
+		console.error("Registration error:", error);
+		res.status(400).json({
+			status: "Bad request",
+			message: "Registration unsuccessful",
+			statusCode: 400,
+		});
+	}
+});
+
+// Login Route
+router.post("/login", async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		// Check if user exists
+		const user = await User.findOne({ where: { email } });
+
+		if (!user) {
+			return res.status(401).json({
 				status: "Bad request",
 				message: "Authentication failed",
 				statusCode: 401,
 			});
 		}
-	});
+
+		// Validate password
+		const hashedPassword = await bcrypt.hash(password, 10);
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+
+		if (!isPasswordValid) {
+			return res.status(401).json({
+				status: "Bad request",
+				message: "Authentication failed",
+				statusCode: 401,
+			});
+		}
+
+		// Generate JWT token
+		const token = jwt.sign(
+			{ userId: user.userId, email: user.email },
+			process.env.JWT_KEY,
+			{ expiresIn: "1h" }
+		);
+
+		// Response with token and user data
+		res.status(200).json({
+			status: "success",
+			message: "Login successful",
+			data: {
+				accessToken: token,
+				user: {
+					userId: user.userId,
+					firstName: user.firstName,
+					lastName: user.lastName,
+					email: user.email,
+					phone: user.phone,
+				},
+			},
+		});
+	} catch (error) {
+		console.error("Login error:", error);
+		res.status(401).json({
+			status: "Bad request",
+			message: "Authentication failed",
+			statusCode: 401,
+		});
+	}
 });
 
 module.exports = router;
